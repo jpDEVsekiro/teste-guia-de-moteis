c86ef0bf0649d0eac1750b758056e97b6b2154f4
João Pedro <devjpdesenvolver@gmail.com>
2025-02-05 20:43:23 -0300

    feat: init app

---
+++ lib/app/modules/motels_listing.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MotelsListing extends StatefulWidget {
+  const MotelsListing({super.key});
+
+  @override
+  State<MotelsListing> createState() => _MotelsListingState();
+}
+
+class _MotelsListingState extends State<MotelsListing> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
