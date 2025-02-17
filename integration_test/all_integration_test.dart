79dacbfe76f22c7cccf3847859e124f7656ae291
João Pedro <devjpdesenvolver@gmail.com>
2025-02-17 09:11:28 -0300

    fix: add main

--- integration_test/all_integration_test.dart
+++ integration_test/all_integration_test.dart
@@ -1,1 +1,6 @@
-void main() {}
+import 'modules/motels_listing/motels_listing_page_integration_test.dart'
+    as motels_listing_page_integration_test;
+
+void main() {
+  motels_listing_page_integration_test.main();
+}
